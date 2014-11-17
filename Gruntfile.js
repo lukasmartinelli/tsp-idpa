71d505c0dbd87dcdd0d3406d245a573965aa1b8d
Lukas Martinelli <me@lukasmartinelli.ch>
2014-11-17 11:14:02 +0100

    Added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-open');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        express: {
+            options: {
+                port: 3000,
+            }
+        },
+        typescript: {
+            base: {
+                src: ['app/**/*.ts'],
+                dest: 'tsp.js',
+                options: {
+                    module: 'amd',
+                    target: 'es5'
+                }
+            }
+        },
+        watch: {
+            files: '**/*.ts',
+            tasks: ['typescript']
+        },
+        open: {
+            dev: {
+                path: 'http://localhost:3000/index.html'
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['express', 'open', 'watch']);
+
+}
